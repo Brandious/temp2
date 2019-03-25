871ac045a5f73d8e9d66199e8fe99edbffce58f6
Brandious <adnanfacic96@gmail.com>
2019-03-25 22:57:42 +0100

    Temp 2

---
+++ js/main.js
@@ -0,0 +1,48 @@
+//let key = AIzaSyBP2r8J-NE5LFiw-gC04JDmXOkSxv7phxE;
+
+let map;
+
+function initMap() {
+
+    
+    const loc = {
+        lat: 44.52,
+        lng: 18.48
+    };
+ 
+    map = new google.maps.Map(document.querySelector('.map'), {
+        zoom: 14, 
+        center: loc
+    });
+
+    const marker = new google.maps.Marker({position: loc, map: map});
+  
+}
+
+window.addEventListener('scroll', function() {
+    if(window.scrollY > 150)
+    {
+        document.querySelector('#navbar').style.opacity = 0.85;
+    }
+    else 
+    {
+        document.querySelector('#navbar').style.opacity = 1;
+    }
+});
+
+$('#navbar a, .btn').on('click', function(event){ 
+   
+    if(this.hash !=='')
+    {
+        event.preventDefault();
+        
+        const hash = this.hash;
+
+        $('html, body').animate(
+            {
+                scrollTop: $(hash).offset().top - 100
+            },
+            800
+        );
+    }
+})
